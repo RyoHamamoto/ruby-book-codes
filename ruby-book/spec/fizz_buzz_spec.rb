524468b23621e1ff5285ab165a841053c509fd7f
test <test@tesutonoMacBook-ea.local>
2019-05-24 13:01:07 +0900

    Rspecの設定追加

--- ruby-book/spec/fizz_buzz_spec.rb
+++ ruby-book/spec/fizz_buzz_spec.rb
@@ -1,1 +1,14 @@
 require './spec/spec_helper.rb'
+require './lib/fizz_buzz'
+
+RSpec.describe 'Fizz Bizz' do
+    it 'returns valid string' do
+        expect(fizz_buzz(1)).to eq '1'
+        expect(fizz_buzz(2)).to eq '2'
+        expect(fizz_buzz(3)).to eq 'Fizz'
+        expect(fizz_buzz(4)).to eq '4'
+        expect(fizz_buzz(5)).to eq 'Buzz'
+        expect(fizz_buzz(6)).to eq 'Fizz'
+        expect(fizz_buzz(15)).to eq 'Fizz Buzz'
+    end
+end
